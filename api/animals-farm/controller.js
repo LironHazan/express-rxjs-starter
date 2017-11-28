51d9c4757bbfc27799397a9ba138a7f2099cc19a
liron <lironenk@gmail.com>
2017-11-28 16:05:46 +0200

    feat(added template engine for simple ui indications)

--- api/animals-farm/controller.js
+++ api/animals-farm/controller.js
@@ -2,6 +2,7 @@
 const AnimalsService = require('./service');
 const animals = require('../MOCK_DATA.json');
 
+// http://localhost:3000/api/animals/fetch
 exports.fetchAllAnimals = (req, res) => {
     const service = new AnimalsService();
     const result = service.simplePager(req.limit, req.pageIndex, animals);
