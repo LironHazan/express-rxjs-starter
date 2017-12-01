f3d3381ad87b7ac2c3eef912e4c3f9602c41c66e
lironh <lironenk@gmail.com>
2017-12-01 20:04:49 +0200

    feat(rxjs)

--- api/animals-farm/controller.js
+++ api/animals-farm/controller.js
@@ -1,13 +1,17 @@
 'use strict';
 const AnimalsService = require('./service');
 const animals = require('../MOCK_DATA.json');
+
 const pageLimit = 25;
+
 // http://localhost:3000/api/animals/fetch
 exports.fetchAllAnimals = (req, res) => {
     const service = new AnimalsService();
     const size = animals.length / pageLimit;
-    const members = service.simplePager(req.query.pageIndex, pageLimit, animals);
-    res.send({members, size});
+    service.simplePager(req.query.pageIndex, pageLimit, animals)
+      .then(members => {
+      res.send({members: members.slice(0, pageLimit), size});
+      }).catch(err => {console.log(err)});
 };
 
 exports.filterBy = (req, res) => {
--- api/animals-farm/service.js
+++ api/animals-farm/service.js
@@ -1,20 +1,39 @@
-class AnimalsService {
+const Rx = require('rxjs');
 
+class AnimalsService {
     constructor(){
 
     }
     // lazy loading, client will ask for x items (limit) it will be the length of the returned array,
     simplePager(pageIndex=0, limit=10, data) {
       const deleteCount =  pageIndex*limit;
-      console.log(data);
-      const animals = data.reduce((acc, value, index) => {
+
+      const source$ = Rx.Observable.from(data)
+      .reduce((acc, value, index) => {
         if(deleteCount <= index) {
           acc.push(value);
         }
         return acc;
       }, []);
-      console.log(animals);
-      return animals.slice(0, limit);
+
+    const observer = {
+      next: i => {console.log(JSON.stringify(i))},
+      error: err => {console.log(err)},
+      complete: () => {}
+    };
+
+    source$.subscribe(observer);
+    return source$.toPromise();
+
+      // console.log(data);
+      // const animals = data.reduce((acc, value, index) => {
+      //   if(deleteCount <= index) {
+      //     acc.push(value);
+      //   }
+      //   return acc;
+      // }, []);
+      // console.log(animals);
+      // return animals.slice(0, limit);
     }
 }
 
