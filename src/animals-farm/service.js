8d5df84820d08f244d434bd062813c4644d8c1a8
lironh <lironenk@gmail.com>
2018-04-25 21:40:31 +0300

    refactor

--- src/animals-farm/service.js
+++ src/animals-farm/service.js
@@ -8,8 +8,7 @@ class AnimalsService {
     simplePager(pageIndex=0, limit=10, data) {
       const deleteCount =  pageIndex*limit;
       const result = this._source.slice(deleteCount, deleteCount+limit);
-      const obs$ = Rx.Observable.of(result);
-      return obs$;
+      return Rx.Observable.of(result);
     }
 
     sortBy(value='age', data) {
